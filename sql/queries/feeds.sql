b9e98b36acd4618e9dcd6bf80d13ca6ffad46837
Fernando Righi <43839098+whatsmynameagain@users.noreply.github.com>
2025-04-12 18:26:54 -0300

    CH5 L1 3

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -45,3 +45,8 @@ UPDATE feeds
 SET last_fetched_at = $2,
     updated_at = $3
 WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at NULLS FIRST
+LIMIT 1;
