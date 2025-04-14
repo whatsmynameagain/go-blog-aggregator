75560ed3b67af323fb5323573c9191f423bffd82
whatsmynameagain <43839098+whatsmynameagain@users.noreply.github.com>
2025-04-14 20:58:08 -0300

    CH5 L2 1

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE posts (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    title TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
+    description TEXT,
+    published_at TIMESTAMP,
+    feed_id UUID REFERENCES feeds(id)
+);
+
+-- +goose Down
+DROP TABLE posts
